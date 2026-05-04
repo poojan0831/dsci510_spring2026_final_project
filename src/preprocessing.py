f1707fca8e618cadc4da4d2e72ff342836af0c4c
poojan0831 <pbrahmbh@usc.edu>
2026-05-04 19:23:57 -0400

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,21 +1,65 @@
-import pandas as pd
+# The main file that runs the entire data analysis pipeline for the Spotify and Billboard datasets.
+# It includes data loading, preprocessing, exploratory data analysis, clustering, PCA analysis, Spotify API calls and Billboard charting analysis.
 
-def checking_for_null_values(df):
-    null_counts = df.isnull().sum()
-    print("Null values in each column:")
-    print(null_counts)
+# For this project, I used AI tools to help improve the efficiency and structure of my code.
+# AI was used selectively to refine certain parts of the code.
+# All instances where AI was used are clearly mentioned in the code comments, specifying where and how it contributed.
 
-def dropping_null_values(df, feauture):
-    df = df.dropna(subset=[feauture])
-    return df
+from data_loader import load_billboard_data, load_spotify_data
+from preprocessing import checking_for_null_values, dropping_null_values, removing_duplicates, correcting_data_types
+from eda_analysis import eda_analysis
+from kmeans_clustering import kmeans
+from pca import pca_analysis
+from spotify_api import spotify_api_calling
+from billboard_charting import billboard_charting
 
-def removing_duplicates(df):
-    df = df.drop_duplicates()
-    return df
+spotify_df = load_spotify_data("../data/songs_normalize.csv")
+billboard_df = load_billboard_data("../data/charts.csv")
 
-def correcting_data_types(spotify_df, billboard_df):
-    spotify_df['year'] = spotify_df['year'].astype(int)
-    billboard_df['date'] = pd.to_datetime(billboard_df['date'])
-    billboard_df['rank'] = billboard_df['rank'].astype(int)
-    billboard_df['artist'] = billboard_df['artist'].str.replace(r'Featuring\..*', '', regex=True)
-    return spotify_df, billboard_df
+# Importing the data
+print(spotify_df.head())
+print(billboard_df.head())
+print("Spotify shape:", spotify_df.shape)
+print("Billboard shape:", billboard_df.shape)
+
+# Preprocessing
+
+# Removing the null values
+print(spotify_df.columns)
+print(billboard_df.columns)
+
+checking_for_null_values(spotify_df)
+checking_for_null_values(billboard_df)
+
+dropping_null_values(billboard_df, "last-week")
+
+
+# Removing duplicates
+removing_duplicates(spotify_df)
+removing_duplicates(billboard_df)
+
+print(billboard_df.shape)
+print(spotify_df.shape)
+
+
+# Correcting the data types
+spotify_df, billboard_df = correcting_data_types(spotify_df, billboard_df)
+
+
+# Data Analysis
+
+# EDA Analysis
+eda_analysis(spotify_df)
+
+# KMeans Clustering
+kmeans(spotify_df)
+
+# PCA Analysis
+pca_analysis(spotify_df)
+
+# Spotify API Calling
+spotify_api_df = spotify_api_calling(spotify_df)
+print(spotify_api_df.head())
+
+# Billboard Charting Comparison
+billboard_charting(billboard_df, spotify_api_df)
